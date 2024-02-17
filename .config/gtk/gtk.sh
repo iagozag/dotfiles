91cc7f9bd4bedf7f40bdff6ad47abca8b1c5648a
iagozag <iagozag@gmail.com>
2024-02-17 01:49:40 -0300

    gtk script added

---
+++ .config/gtk/gtk.sh
@@ -0,0 +1,4 @@
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Ice'
+gsettings set org.gnome.desktop.interface font-name 'Fira Code 12'
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
